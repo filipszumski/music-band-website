95c18f4324a30725cb7be27b4c32bf4a2ee2d0c2
Filip Szumski <filipszumski@gmail.com>
2021-07-13 23:06:37 +0200

    done basic html structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <div>
+        <header>
+          <h1>Fisz Emade Tworzywo</h1>
+        </header>
+        <nav>
+          <ul>
+            <li>O nas</li>
+            <li>Video</li>
+            <li>Kontakt</li>
+          </ul>
+        </nav>
+      </div>
+      <main>
+        <section>
+          <h2>O nas</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis reprehenderit fuga, cum vitae nihil ipsa ducimus optio harum sint dolore culpa officiis id ullam nesciunt aut, odit illo, dolorum omnis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae ducimus laudantium accusamus natus eius sed. Iste quos cum provident molestias eligendi, inventore aut unde quia reprehenderit. Laborum nam porro ut. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam distinctio architecto quae molestias. In sunt sed adipisci! Nisi dolore omnis, accusantium, animi libero blanditiis non aliquam deleniti magnam, quos sed. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate nam expedita mollitia ipsam, minima ducimus accusantium tenetur beatae quia tempore exercitationem quae voluptatum amet incidunt fuga, eum ab, reprehenderit alias!
+          </p>
+        </section>
+        <section>
+          <h2>Video</h2>
+          <p>Video #1</p>
+          <p>Video #2</p>
+        </section>
+      </main>
+    </>
   );
 }
 
