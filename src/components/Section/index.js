fe1141767ae94296cced6575e0aec90710dd96f0
Filip Szumski <filipszumski@gmail.com>
2021-07-14 00:54:21 +0200

    passed id prop to Section for links

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-export const Section = ({ title, body }) => {
+export const Section = ({ title, body, id }) => {
 
     return (
         <section>
-            <h2>
+            <h2 id={id}>
                 {title}
             </h2>
             <div>
