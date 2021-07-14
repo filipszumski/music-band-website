59cb7585b512ef91b8dfa439cb6f4e2daeec884a
Filip Szumski <filipszumski@gmail.com>
2021-07-15 00:23:16 +0200

    added handleClick to close form button, FormContainerWrapper, submit button

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,28 +1,32 @@
 import React from "react";
-import { StyledForm } from "./styled";
+import { FormContentWrapper, StyledForm } from "./styled";
 
-export const Form = () => {
+export const Form = ({ isFormActive, setIsFormActive }) => {
+
+    const handleClick = () => {
+        setIsFormActive(prevState => !prevState)
+    };
 
     return (
-        <StyledForm>
-            <button>Close</button>
-            <p>
-                <label htmlFor="name">Imię i nazwisko</label>
-                <input type="text" name="name" id="name" />
-            </p>
-            <p>
-                <label htmlFor="email">E-mail</label>
-                <input type="email" name="email" id="email" />
-            </p>
-            <p>
-                <label htmlFor="tel">Telefon</label>
-                <input type="tel" name="tel" id="tel" />
-            </p>
-            <p>
-                <label htmlFor="message">Treść wiadomości</label>
-                <textarea name="tel" id="tel" />
-            </p>
-            <p>
+        <StyledForm isActive={isFormActive}>
+            <FormContentWrapper>
+                <button onClick={handleClick}>Close</button>
+                <p>
+                    <label htmlFor="name">Imię i nazwisko</label>
+                    <input type="text" name="name" id="name" />
+                </p>
+                <p>
+                    <label htmlFor="email">E-mail</label>
+                    <input type="email" name="email" id="email" />
+                </p>
+                <p>
+                    <label htmlFor="tel">Telefon</label>
+                    <input type="tel" name="tel" id="tel" />
+                </p>
+                <p>
+                    <label htmlFor="message">Treść wiadomości</label>
+                    <textarea name="tel" id="tel" />
+                </p>
                 <fieldset>
                     <legend>Weryfikacja użytkownika (odpowiedz prawidłowo na pytanie)</legend>
                     <p>
@@ -41,7 +45,8 @@ export const Form = () => {
                         <label htmlFor="answer_1">Odpowiedz_3</label>
                     </p>
                 </fieldset>
-            </p>
+                <button type="submit">Wyślij</button>
+            </FormContentWrapper>
         </StyledForm>
     )
 }
--- src/components/Form/styled.js
+++ src/components/Form/styled.js
@@ -1,15 +1,32 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const StyledForm = styled.form`
     position: fixed;
     top: 0px;
     right: 0px;
     height: 100%;
-    width: 50%;
-    color: white;
-    background-color: black;
+    width: 50vw;
+    color: #eeee;
+    background-color: #1A1A1A  ;
     overflow-y: auto;
-    padding: 20px;
+    padding: 25px;
     display: none;
     z-index: 2;
+    animation-name: rightSideEject;
+    animation-duration: 0.5s;
+
+    @keyframes rightSideEject {
+        from {right: -50vw; opacity: 0}
+        to {right: 0px; opacity: 1}
+    }
+
+    ${({ isActive }) => isActive && css`
+        display:block;
+    `}
+`;
+
+export const FormContentWrapper = styled.div`
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 15px;
 `;
